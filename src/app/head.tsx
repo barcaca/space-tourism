e5ae38078c3b3a498b043275a45f40645845c18c
Luan Barcaça <73846225+Barcaca@users.noreply.github.com>
2023-12-23 22:14:18 -0300

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -13,7 +13,7 @@ export default function Head() {
       <meta name="robots" content="index, follow" />
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no, user-scalable=no"
       />
       <meta property="og:title" content="Space Tourism" />
       <meta
